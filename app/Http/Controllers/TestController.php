8fe19347824753e407eabe127efb428ec3e924e3
Azangue Ulrich <83842067+azangue-cmd@users.noreply.github.com>
2023-09-02 14:00:55 +0100

    initial commit

---
+++ app/Http/Controllers/TestController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class TestController extends Controller {
+      
+    public function index(){
+        return[
+
+            "code"=>0,
+            "data"=>'We have a lot of data',
+            "msg"=>'Welcome azangue' 
+            
+        ];
+    }
+}
